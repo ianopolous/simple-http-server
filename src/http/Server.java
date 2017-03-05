dce5e71341bbf9ee480c36943556fb5e8548d716
Ian Preston <ianopolous@gmail.com>
2017-03-05 21:08:53 +0000

    Add help

--- src/http/Server.java
+++ src/http/Server.java
@@ -8,6 +8,10 @@ import java.net.*;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
+	    if (args.length < 1 || args[0].equals("-help") || args[0].equals("--help")) {
+            System.out.println("Usage: java -jar HttpServer.jar $webroot [$port]");
+            return;
+        }
 		HttpServer httpServer = HttpServer.create();
 		httpServer.createContext("/", new StaticHandler(args[0], false, false));
 		int port = args.length > 1 ? Integer.parseInt(args[1]) : 8000;
