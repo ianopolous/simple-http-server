cb423605f937835e369882f14e156e53e4333b9f
ian <ianopolous@protonmail.com>
2021-11-24 12:49:28 +0000

    Simplify, remove caching, and gzip

--- src/http/Server.java
+++ src/http/Server.java
@@ -8,13 +8,12 @@ import java.net.*;
 public class Server {
 
 	public static void main(String[] args) throws IOException {
-	    if (args.length < 2 || args[0].equals("-help") || args[0].equals("--
-help")) {
-		System.out.println("Usage: java -jar HttpServer.jar $web_root $port");
-		return;
-	    }
+		if (args.length < 2 || args[0].equals("-help") || args[0].equals("--help")) {
+			System.out.println("Usage: java -jar HttpServer.jar $web_root $port");
+			return;
+		}
 	    HttpServer httpServer = HttpServer.create();
-	    httpServer.createContext("/", new StaticHandler(args[0], false, false));
+	    httpServer.createContext("/", new StaticHandler(args[0]));
 	    int port = Integer.parseInt(args[1]);
 	    httpServer.bind(new InetSocketAddress("localhost", port), 100);
 	    httpServer.start();
--- src/http/StaticHandler.java
+++ src/http/StaticHandler.java
@@ -10,20 +10,14 @@ import java.util.zip.*;
 
 public class StaticHandler implements HttpHandler
 {
-    private static Map<String, Asset> data = new HashMap<>();
-    private final boolean caching, gzip;
     private final String pathToRoot;
 
-    public StaticHandler(String pathToRoot, boolean caching, boolean gzip) throws IOException {
-        this.caching = caching;
+    public StaticHandler(String pathToRoot) {
         this.pathToRoot = pathToRoot.endsWith("/") ? pathToRoot : pathToRoot + "/";
-        this.gzip = gzip;
 
-        File[] files = new File(pathToRoot).listFiles();
-        if (files == null)
+        File base = new File(pathToRoot);
+        if (! base.exists() || ! base.isDirectory())
             throw new IllegalStateException("Couldn't find webroot: "+pathToRoot);
-        for (File f: files)
-            processFile("", f, gzip);
     }
 
     private static class Asset {
@@ -43,19 +37,13 @@ public class StaticHandler implements HttpHandler
             if (path.length() == 0)
                 path = "index.html";
 
-	    boolean fromFile = new File(pathToRoot + path).exists();
-	    InputStream in = fromFile ? new FileInputStream(pathToRoot + path)
-		: ClassLoader.getSystemClassLoader().getResourceAsStream(pathToRoot + path);
-	    if (in == null) {
-		String toAsset = pathToRoot + path + ".html";
-		fromFile = new File(toAsset).exists();
-		in = fromFile ? new FileInputStream(toAsset)
-                    : ClassLoader.getSystemClassLoader().getResourceAsStream(toAsset);
-	    }
-            Asset res = caching ? data.get(path) : new Asset(readResource(in, gzip));
+            File file = new File(pathToRoot + path);
+            InputStream in = new FileInputStream(file);
+            if (in == null) {
+                String toAsset = pathToRoot + path + ".html";
+                file = new File(toAsset);
+            }
 
-            if (gzip)
-                httpExchange.getResponseHeaders().set("Content-Encoding", "gzip");
             if (path.endsWith(".js"))
                 httpExchange.getResponseHeaders().set("Content-Type", "text/javascript");
             else if (path.endsWith(".html"))
@@ -67,123 +55,27 @@ public class StaticHandler implements HttpHandler
             else if (path.endsWith(".svg"))
                 httpExchange.getResponseHeaders().set("Content-Type", "image/svg+xml");
             if (httpExchange.getRequestMethod().equals("HEAD")) {
-                httpExchange.getResponseHeaders().set("Content-Length", "" + res.data.length);
+                httpExchange.getResponseHeaders().set("Content-Length", "" + file.length());
                 httpExchange.sendResponseHeaders(200, -1);
                 return;
             }
 
-            httpExchange.sendResponseHeaders(200, res.data.length);
-            httpExchange.getResponseBody().write(res.data);
-            httpExchange.getResponseBody().close();
+            httpExchange.sendResponseHeaders(200, file.length());
+            OutputStream body = httpExchange.getResponseBody();
+            byte[] tmp = new byte[4096];
+            int r;
+            while ((r=in.read(tmp)) >= 0)
+                body.write(tmp, 0, r);
+            body.flush();
+            body.close();
         } catch (NullPointerException t) {
             System.err.println("Error retrieving: " + path);
-	    t.printStackTrace();
+            t.printStackTrace();
 	    httpExchange.sendResponseHeaders(404, 0);
             httpExchange.getResponseBody().close();
         } catch (Throwable t) {
             System.err.println("Error retrieving: " + path);
             t.printStackTrace();
         }
-    }
-
-    private static void processFile(String path, File f, boolean gzip) throws IOException {
-        if (!f.isDirectory())
-            data.put(path + f.getName(), new Asset(readResource(new FileInputStream(f), gzip)));
-        if (f.isDirectory())
-            for (File sub: f.listFiles())
-                processFile(path + f.getName() + "/", sub, gzip);
-    }
-
-    private static byte[] readResource(InputStream in, boolean gzip) throws IOException {
-        ByteArrayOutputStream bout = new ByteArrayOutputStream();
-        OutputStream gout = gzip ? new GZIPOutputStream(bout) : new DataOutputStream(bout);
-        byte[] tmp = new byte[4096];
-        int r;
-        while ((r=in.read(tmp)) >= 0)
-            gout.write(tmp, 0, r);
-        gout.flush();
-        gout.close();
-        in.close();
-        return bout.toByteArray();
-    }
-
-    private static List<String> getResources(String directory)
-    {
-        ClassLoader context = Thread.currentThread().getContextClassLoader();
-
-        List<String> resources = new ArrayList<>();
-
-        ClassLoader cl = StaticHandler.class.getClassLoader();
-        if (!(cl instanceof URLClassLoader))
-            throw new IllegalStateException();
-        URL[] urls = ((URLClassLoader) cl).getURLs();
-
-        int slash = directory.lastIndexOf("/");
-        String dir = directory.substring(0, slash + 1);
-        for (int i=0; i<urls.length; i++)
-        {
-            if (!urls[i].toString().endsWith(".jar"))
-                continue;
-            try
-            {
-                JarInputStream jarStream = new JarInputStream(urls[i].openStream());
-                while (true)
-                {
-                    ZipEntry entry = jarStream.getNextEntry();
-                    if (entry == null)
-                        break;
-                    if (entry.isDirectory())
-                        continue;
-
-                    String name = entry.getName();
-                    slash = name.lastIndexOf("/");
-                    String thisDir = "";
-                    if (slash >= 0)
-                        thisDir = name.substring(0, slash + 1);
-
-                    if (!thisDir.startsWith(dir))
-                        continue;
-                    resources.add(name);
-                }
-
-                jarStream.close();
-            }
-            catch (IOException e) { e.printStackTrace();}
-        }
-        InputStream stream = context.getResourceAsStream(directory);
-        try
-        {
-            if (stream != null)
-            {
-                StringBuilder sb = new StringBuilder();
-                char[] buffer = new char[1024];
-                try (Reader r = new InputStreamReader(stream))
-                {
-                    while (true)
-                    {
-                        int length = r.read(buffer);
-                        if (length < 0)
-                        {
-                            break;
-                        }
-                        sb.append(buffer, 0, length);
-                    }
-                }
-
-                for (String s : sb.toString().split("\n"))
-                {
-                    if (s.length() > 0 && context.getResource(directory + s) != null)
-                    {
-                        resources.add(s);
-                    }
-                }
-            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        return resources;
     }
 }
